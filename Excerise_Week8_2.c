998a496de49f030e1a1ef54556ab57d5c4668315
OOT OOT <ootcazy007@gmail.com>
2021-10-10 12:56:45 +0700

    week 8

--- Excerise_Week8_2.c
+++ Excerise_Week8_2.c
@@ -1,12 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
 int main()
 {
     int x,y;
-    scanf ("%d %d" ,&x,&y);
+    printf ("x : ");
+    scanf ("%d" ,&x);
+    printf ("y : ");
+    scanf ("%d" ,&y);
     x = x * y;
     y = x / y;
     x = x / y;
-    printf ("%d %d" ,x,y);
+    printf ("x = %d, y = %d" ,x,y);
     return 0;
 }
